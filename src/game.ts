f005a04ff73756375f2ee2f88fdb9548e0be2804
Steven Taylor <steven.taylor788@googlemail.com>
2022-10-03 19:51:57 +0000

    adds initial files

---
+++ src/game.ts
@@ -0,0 +1,13 @@
+import * as ex from 'excalibur';
+export class Game extends ex.Scene {
+    random = new ex.Random(1337); // seeded random
+
+    constructor() {
+        super();
+    }
+
+    onInitialize(engine: ex.Engine) {
+        
+    }
+
+}
